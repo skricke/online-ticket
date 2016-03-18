9008057dd8d2af3f1aa89273ebf10b123748075a
Sebastian Kricke <s0544316@htw-berlin.de>
2016-03-18 14:42:46 +0100

    models, validations and relations created

---
+++ app/models/event.rb
@@ -0,0 +1,14 @@
+class Event < ActiveRecord::Base
+  validates :title,
+            presence: true,
+            message: "Die Veranstaltung muss einen Namen haben."
+  validates :description,
+            length: { maximum: 3000,
+                      too_long: "Die Beschreibung darf maximal 3000 Zeichen haben."}
+  validates :price,
+            presence: true,
+            numericality: { greater_than: 0},
+            message: "Es muss ein Preis größer 0 festgelegt sein."
+  belongs_to :venue
+  has_many :orders
+end
